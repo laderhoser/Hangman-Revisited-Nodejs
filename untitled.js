e0b593389a5fe3a25de6ba942486e90d9a00868a
Jose Martinez <josemartinez@Joses-MacBook-Pro.local>
2017-01-30 02:11:33 -0600

    added more finish touches

---
+++ untitled.js
@@ -0,0 +1,109 @@
+
+            }if (guessCount == 8) {
+                console.log(" ________    ")
+                console.log(" _________   ")
+                console.log("|/       |   ")
+                console.log("|            ")
+                console.log("|            ")
+                console.log("|            ");
+                console.log("|            ");
+                console.log("|            ");
+                console.log("|_______.    ");
+                star();
+            }  if (guessCount == 7) {
+                console.log(" ________    ")
+                console.log(" _________   ")
+                console.log("|/       |   ")
+                console.log("|       (_)  ")
+                console.log("|            ")
+                console.log("|            ");
+                console.log("|            ");
+                console.log("|            ");
+                console.log("|_______.    ");
+                startGame();
+            }  if (guessCount == 6) {
+                console.log(" ________    ")
+                console.log(" _________   ")
+                console.log("|/       |   ")
+                console.log("|       (_)  ")
+                console.log("|        |   ")
+                console.log("|            ");
+                console.log("|            ");
+                console.log("|            ");
+                console.log("|_______.    ");
+                startGame();
+            }   if (guessCount == 5) {
+                console.log(" ________    ")
+                console.log(" _________   ")
+                console.log("|/       |   ")
+                console.log("|       (_)  ")
+                console.log("|       \|   ")
+                console.log("|            ");
+                console.log("|            ");
+                console.log("|            ");
+                console.log("|_______.    ");
+                startGame();
+            }
+
+           
+         
+           
+            if (guessCount == 4) {
+                console.log(" ________    ")
+                console.log(" _________   ")
+                console.log("|/       |   ")
+                console.log("|       (_)  ")
+                console.log("|       \|/  ")
+                console.log("|            ");
+                console.log("|            ");
+                console.log("|            ");
+                console.log("|_______.    ");
+                startGame();
+            }
+          if (guessCount == 3) {
+                console.log(" ________    ")
+                console.log(" _________   ")
+                console.log("|/       |   ")
+                console.log("|       (_)  ")
+                console.log("|       \|/  ")
+                console.log("|        |   ");
+                console.log("|            ");
+                console.log("|            ");
+                console.log("|_______.    ");
+                startGame();
+
+            } 
+          
+               if (guessCount == 2) {
+                console.log(" ________    ")
+                console.log(" _________   ")
+                console.log("|/       |   ")
+                console.log("|       (_)  ")
+                console.log("|       \|/  ")
+                console.log("|        |   ");
+                console.log("|       /    ");
+                console.log("|            ");
+                console.log("|_______.    ");
+
+                startGame();
+
+
+
+            }
+            if (guessCount == 1) {
+                console.log(" ________    ")
+                console.log(" _________   ")
+                console.log("|/       |   ")
+                console.log("|       (_)  ")
+                console.log("|       \|/  ")
+                console.log("|        |   ");
+                console.log("|       / \  ");
+                console.log("|            ");
+                console.log("|_______.    ");
+
+
+                startGame();
+
+
+
+            }
